0097ddfa92dec2b51347bef82ce938f562f7311d
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-01-24 22:28:56 +0530

    Update LoginSignup.jsx
    Redirecting from login/signup to home page.

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -15,9 +15,45 @@ const LoginSignup = () => {
 
   const login = async () => {
     console.log("Login Function", formData);
+    let responseData;
+    await fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data));
+
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
   };
   const signup = async () => {
     console.log("SignUp Function", formData);
+    let responseData;
+    await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => (responseData = data));
+
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
   };
 
   return (
@@ -26,12 +62,30 @@ const LoginSignup = () => {
         <h1>{state}</h1>
         <div className="loginsignup-fields">
           {state === "Sign Up" ? (
-            <input name="username" value={formData.username} onChange={changeHandler} type="text" placeholder="Your name" />
+            <input
+              name="username"
+              value={formData.username}
+              onChange={changeHandler}
+              type="text"
+              placeholder="Your name"
+            />
           ) : (
             <></>
           )}
-          <input name="email" value={formData.email} onChange={changeHandler} type="email" placeholder="Email Address" />
-          <input name="password" value={formData.password} onChange={changeHandler} type="password" placeholder="Password" />
+          <input
+            name="email"
+            value={formData.email}
+            onChange={changeHandler}
+            type="email"
+            placeholder="Email Address"
+          />
+          <input
+            name="password"
+            value={formData.password}
+            onChange={changeHandler}
+            type="password"
+            placeholder="Password"
+          />
         </div>
         <button
           onClick={() => {
